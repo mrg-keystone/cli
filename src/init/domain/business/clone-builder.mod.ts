9f6f4ac9f58124233da4de85b696e15ffb4e0f75
Raphael Castro <rafac@monsterrg.com>
2026-01-22 14:30:08 -0500

    Add init command to clone keystone-suite repositories
    - Clone backend, ui, prototypes, cli, docs from mrg-keystone org
    - Organized in layered architecture (data/business/coordinators)
    - Creates keystone-suite directory at specified path

---
+++ src/init/domain/business/clone-builder.mod.ts
@@ -0,0 +1,25 @@
+import type { CloneRequest } from "@init/domain/data/git-client.mod.ts";
+
+export const REPOS = [
+  "https://github.com/mrg-keystone/backend",
+  "https://github.com/mrg-keystone/ui",
+  "https://github.com/mrg-keystone/prototypes",
+  "https://github.com/mrg-keystone/cli",
+  "https://github.com/mrg-keystone/docs",
+];
+
+export function extractRepoName(url: string): string {
+  return url.split("/").pop()!;
+}
+
+export function buildTargetDir(basePath: string): string {
+  return `${basePath}/keystone-suite`;
+}
+
+export function buildCloneRequests(repos: string[], targetDir: string): CloneRequest[] {
+  return repos.map((repoUrl) => ({
+    repoUrl,
+    repoName: extractRepoName(repoUrl),
+    targetDir,
+  }));
+}
