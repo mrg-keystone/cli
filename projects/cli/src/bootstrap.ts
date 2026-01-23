129331273c12830a09b857f06d235136fb495aec
Raphael Castro <rafac@monsterrg.com>
2026-01-23 14:51:11 -0500

    Bump version to 0.1.11

--- projects/cli/src/bootstrap.ts
+++ projects/cli/src/bootstrap.ts
@@ -5,7 +5,7 @@ import { init } from "@init/mod.ts";
 await new Command()
   .name("keystone")
   .description("Keystone CLI")
-  .version("0.1.7")
+  .version("0.1.11")
   .action(function () {
     this.showHelp();
   })
